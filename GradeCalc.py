51c10e48181a2ff550ba034db6aebb031544db77
CHFahy <ciaran@ciaranfahy.com>
2021-02-04 18:38:10 +0000

    improved error handling

--- GradeCalc.py
+++ GradeCalc.py
@@ -2,29 +2,32 @@
 #MIT License
 
 def main(grades, maxgrades, weights):
-    """Input; 3 lists with weight as multiplier, Returns; grades explained(str)"""
+    """Input; 3 lists with weight as multiplier, Returns; decimal(float) and weightedgrade(float)"""
     try:
         if len(grades) != len(weights) or len(weights) != len(maxgrades):
             return("Len of grades and weights not equal")
 
-        high = weightedgrade(maxgrades, weights);
-        grade = weightedgrade(grades, weights);
-        percentage = grade / high * 100
-
-        return('Your Weighted Grade is ' + str(grade) + ' which is ' + str(percentage) + '%')
+        high = weightgrade(maxgrades, weights);
+        weightedgrade = weightgrade(grades, weights);
+        decimal = weightedgrade / high
+        return(decimal, weightedgrade)
 
     except ZeroDivisionError:
-        return('Zero Division')
+        print('Zero Division')
+        return()
+    except TypeError:
+        print('Type Error')
+        return()
     
 
-def weightedgrade(grades, weights):
+def weightgrade(grades, weights):
     """Input; 2 lists, Returns; weighted grade(int)"""
     
     weightedgrade = 0
     for i in range (0, len(grades)):
-        weightedgrade = weightedgrade + grades[i] * weights[i]
+        weightgrade = weightedgrade + grades[i] * weights[i]
 
-    return(weightedgrade)
+    return(weightgrade)
 
 #a = main([16.7, 40.41, 58.65], [20, 50, 80], [0.1, 0.4, 0.5]);
 #print(a)
--- RunInput.py
+++ RunInput.py
@@ -1,12 +1,21 @@
 import GradeCalc
 def main():
-    n = int(input("Enter Number of Assessements:\n"))
-    grades = []
-    maxgrades = []
-    weights = []
-    for i in range (0, n):
-        grades.append(int(input("Enter Grade for Assessment " + str(n) + ":\n")))
-        maxgrades.append(int(input("Enter MaxGrade for Assessment " + str(n) + ":\n")))
-        weights.append(int(input("Enter Weight for Assessment " + str(n) + " as multiplier(eg. 0.1):\n")))
-    return(GradeCalc.main(grades, maxgrades, weights))
-print(main())
+    try:
+        n = int(input("Enter Number of Assessements:\n"))
+        grades = []
+        maxgrades = []
+        weights = []
+        for i in range (0, n):
+            grades.append(float(input("Enter Marks for Assessment " + str(i+1) + ":\n")))
+            maxgrades.append(float(input("Enter Maximum Marks for Assessment " + str(i+1) + ":\n")))
+            weights.append(float(input("Enter Weight for Assessment " + str(i+1) + 
+            " as multiplier(eg. 0.1 for 10%):\n")))
+
+        decimal, weightedgrade = GradeCalc.main(grades, maxgrades, weights)
+        print('Your Weighted Grade is ' + str(weightedgrade) + ' which is ' + str(decimal*100) + '%')
+        return(decimal, weightedgrade)
+        
+    except TypeError:
+        print('invalid input')
+        return()
+main()
